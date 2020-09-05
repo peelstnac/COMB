4a1d8921c7994259c5a79ab4405f5503c5990c8b
peelstnac <chfrn2002@gmail.com>
2020-09-05 17:28:38 -0400

    feat: added toggle autoscroll button to dashboard

--- src/platform/client/src/components/dashboard.js
+++ src/platform/client/src/components/dashboard.js
@@ -15,8 +15,10 @@ class Dashboard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            console: []
+            console: [],
+            autoScroll: "Turn off autoscroll"
         }
+        this.handleAutoScroll = this.handleAutoScroll.bind(this);
     }
     componentDidMount() {
         axios.get(method + 'localhost:4000/auth/socket', { withCredentials: true }).then((res) => {
@@ -28,24 +30,44 @@ class Dashboard extends React.Component {
                 socket.emit('secret', data.secret);
                 socket.on('console', (data) => {
                     this.setState(({ console }) => ({
-                        console: [data, ...console]
+                        console: [...console, data]
                     }));
                     console.log(1);
                 });
             }
         });
     }
+    componentDidUpdate() {
+        if (this.state.autoScroll === "Turn off autoscroll") this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+    }
+    handleAutoScroll() {
+        this.setState((state) => {
+            var buttonText = "";
+            if (this.state.autoScroll === "Turn off autoscroll") {
+                buttonText = "Turn on autoscroll";
+            } else {
+                buttonText = "Turn off autoscroll";
+            }
+            return {
+                autoScroll: buttonText
+            };
+        })
+    }
     render() {
-        return(
+        return (
             <>
                 <h1>Dashboard</h1>
                 <div className="console">
                     {this.state.console.map((e, i) => {
                         return <div key={i}>{e}</div>;
                     })}
+                    <div
+                        ref={(el) => { this.messagesEnd = el; }}>
+                    </div>
                 </div>
+                <button onClick={this.handleAutoScroll}>{this.state.autoScroll}</button>
             </>
-        );        
+        );
     }
 
 }
