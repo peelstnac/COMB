331b951af34149d723d606217d1e92bf872fc3f5
peelstnac <chfrn2002@gmail.com>
2020-09-06 10:54:36 -0400

    fix: fixed some dashboard lag by actually assigning proper keys to divs

--- src/platform/client/src/components/dashboard.js
+++ src/platform/client/src/components/dashboard.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import axios from 'axios';
 import io from 'socket.io-client';
-import { v4 as uuidv4 } from 'uuid';
 // Import CSS
 import './shared.css';
 import './dashboard.css';
@@ -22,6 +21,12 @@ class Dashboard extends React.Component {
         }
         this.handleAutoScroll = this.handleAutoScroll.bind(this);
         this.handleBack = this.handleBack.bind(this);
+
+        this.keyCount = 0;
+        this.getKey = this.getKey.bind(this);
+    }
+    getKey() {
+        return this.keyCount++;
     }
     componentDidMount() {
         axios.get(method + 'localhost:4000/auth/socket', { withCredentials: true }).then((res) => {
@@ -40,13 +45,13 @@ class Dashboard extends React.Component {
                         if (tokens.length === 1) {
                             switch(tokens[0]) {
                                 case "0":
-                                    res.push(<div key={uuidv4()} className="ethernet-title">Ethernet Header--------------------------------</div>);
+                                    res.push(<div key={this.getKey()} className="ethernet-title">Ethernet Header--------------------------------</div>);
                                     break;
                                 case "1":
-                                    res.push(<div key={uuidv4()} className="ip-title">IPv4 Header------------------------------------</div>);
+                                    res.push(<div key={this.getKey()} className="ip-title">IPv4 Header------------------------------------</div>);
                                     break;
                                 case "2":
-                                    res.push(<div key={uuidv4()} className="tcp-title">TCP Header-------------------------------------</div>);
+                                    res.push(<div key={this.getKey()} className="tcp-title">TCP Header-------------------------------------</div>);
                                     break;
                                 default:
                                     
@@ -56,91 +61,91 @@ class Dashboard extends React.Component {
                         }
                         switch (tokens[0]) {
                             case "0.1":
-                                res.push(<div key={uuidv4}><span className="dest">Destination MAC Address:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="dest">Destination MAC Address:</span> {tokens[1]}</div>);
                                 break;
                             case "0.2":
-                                res.push(<div key={uuidv4}><span className="src">Source MAC Address:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="src">Source MAC Address:</span> {tokens[1]}</div>);
                                 break;
                             case "0.3":
-                                res.push(<div key={uuidv4}><span className="tag">Protocol ID:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="tag">Protocol ID:</span> {tokens[1]}</div>);
                                 break;
                             case "1.1":
-                                res.push(<div key={uuidv4}><span className="tag">Header length:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="tag">Header length:</span> {tokens[1]}</div>);
                                 break;
                             case "1.2":
-                                res.push(<div key={uuidv4}><span className="tag">IP Version:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="tag">IP Version:</span> {tokens[1]}</div>);
                                 break;
                             case "1.3":
-                                res.push(<div key={uuidv4}><span className="dest">Destination:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="dest">Destination:</span> {tokens[1]}</div>);
                                 break;
                             case "1.4":
-                                res.push(<div key={uuidv4}><span className="src">Source:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="src">Source:</span> {tokens[1]}</div>);
                                 break;
                             case "1.5":
-                                res.push(<div key={uuidv4}><span className="tag">Type of Service:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="tag">Type of Service:</span> {tokens[1]}</div>);
                                 break;
                             case "1.6":
-                                res.push(<div key={uuidv4}><span className="tag">Total Length:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="tag">Total Length:</span> {tokens[1]}</div>);
                                 break;
                             case "1.7":
-                                res.push(<div key={uuidv4}><span className="tag">Header length:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="tag">Header length:</span> {tokens[1]}</div>);
                                 break;
                             case "1.8":
-                                res.push(<div key={uuidv4}><span className="tag">Time to Live:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="tag">Time to Live:</span> {tokens[1]}</div>);
                                 break;
                             case "1.9":
-                                res.push(<div key={uuidv4}><span className="tag">Protocol:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="tag">Protocol:</span> {tokens[1]}</div>);
                                 break;
                             case "1.10":
-                                res.push(<div key={uuidv4}><span className="tag">Checksum:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="tag">Checksum:</span> {tokens[1]}</div>);
                                 break;
                             case "2.1":
-                                res.push(<div key={uuidv4}><span className="dest">Destination Port:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="dest">Destination Port:</span> {tokens[1]}</div>);
                                 break;
                             case "2.2":
-                                res.push(<div key={uuidv4}><span className="src">Source Port:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="src">Source Port:</span> {tokens[1]}</div>);
                                 break;
                             case "2.3":
-                                res.push(<div key={uuidv4}><span className="tag">seq:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="tag">seq:</span> {tokens[1]}</div>);
                                 break;
                             case "2.4":
-                                res.push(<div key={uuidv4}><span className="tag">ack_seq:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="tag">ack_seq:</span> {tokens[1]}</div>);
                                 break;
                             case "2.5":
-                                res.push(<div key={uuidv4}><span className="tag">res1:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="tag">res1:</span> {tokens[1]}</div>);
                                 break;
                             case "2.6":
-                                res.push(<div key={uuidv4}><span className="tag">res2:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="tag">res2:</span> {tokens[1]}</div>);
                                 break;
                             case "2.7":
-                                res.push(<div key={uuidv4}><span className="tag">doff:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="tag">doff:</span> {tokens[1]}</div>);
                                 break;
                             case "2.8":
-                                res.push(<div key={uuidv4}><span className="tag">fin:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="tag">fin:</span> {tokens[1]}</div>);
                                 break;
                             case "2.9":
-                                res.push(<div key={uuidv4}><span className="tag">syn:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="tag">syn:</span> {tokens[1]}</div>);
                                 break;
                             case "2.10":
-                                res.push(<div key={uuidv4}><span className="tag">rst:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="tag">rst:</span> {tokens[1]}</div>);
                                 break;
                             case "2.11":
-                                res.push(<div key={uuidv4}><span className="tag">psh:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="tag">psh:</span> {tokens[1]}</div>);
                                 break;
                             case "2.12":
-                                res.push(<div key={uuidv4}><span className="tag">ack:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="tag">ack:</span> {tokens[1]}</div>);
                                 break;
                             case "2.13":
-                                res.push(<div key={uuidv4}><span className="tag">urg:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="tag">urg:</span> {tokens[1]}</div>);
                                 break;
                             case "2.14":
-                                res.push(<div key={uuidv4}><span className="tag">window:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="tag">window:</span> {tokens[1]}</div>);
                                 break;
                             case "2.15":
-                                res.push(<div key={uuidv4}><span className="tag">check:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="tag">check:</span> {tokens[1]}</div>);
                                 break;
                             case "2.16":
-                                res.push(<div key={uuidv4}><span className="tag">urg_ptr:</span> {tokens[1]}</div>);
+                                res.push(<div key={this.getKey()}><span className="tag">urg_ptr:</span> {tokens[1]}</div>);
                                 break;
                         }
                     }
